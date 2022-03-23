7aa69ff602e8a687dd698547499aa6f1f9772af1
alfazidan <alfazidan76@gmail.com>
2022-03-23 10:30:01 +0700

    inisialisasi project taxi

---
+++ Taxi.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasClassDanObject
+{
+    class Taxi
+    {
+        //properties
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public float NumPassenger { get; set; }
+
+        //method
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name : {0}", DriverName);
+            if (OnDuty == true)
+            {
+                Console.WriteLine("On Duty : yes");
+            }
+            else
+            {
+                Console.WriteLine("On Duty : no");
+            }
+            Console.WriteLine("Number of Passenger : {0}", NumPassenger);
+        }
+
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("{0} sedang menjemput penumpang\n", DriverName);
+        }
+
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} selesai menjemput penumpang\n", DriverName);
+        }
+    }
+}
